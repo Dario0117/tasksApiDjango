7412d03b3e41875fd1a1efd21e03ec27300a22e3
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-07-29 09:20:03 -0500

    renamed app

--- tasks/tests.py
+++ tasks/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase, Client
 
 from .views import getDict
 
-class LocalAuthTestCase(TestCase):
+class TasksTestCase(TestCase):
 
     def setUp(self):
         self.makeRequest = Client()
